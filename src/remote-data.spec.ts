13934eeaf622595ed6b988ade0434eae85cdb7c7
Mike Whittom <mikewhittom27@gmail.com>
2021-11-12 11:27:36 -0500

    add map, map2, withDefault and documentation and exemples and fix some type

--- src/remote-data.spec.ts
+++ src/remote-data.spec.ts
@@ -1,20 +1,38 @@
-import { RemoteData, Loading, loading, fold, success, failure, notAsked } from "./remote-data";
+import { loading, fold, success, failure, notAsked, RemoteData, map, map2, withDefault } from "./remote-data";
 
 describe("RemoteData", () => {
+  describe("map", () => {
+    describe("when Succeed", () => it("should return Success", () => expect(map((x) => +x)(success("10"))).toEqual(success(10))));
+    describe("when NotAsked", () => it("should return NotAsked", () => expect(map((x) => +x)(notAsked())).toEqual(notAsked())));
+    describe("when Failure", () => it("should return Failure", () => expect(map((x) => +x)(failure("10"))).toEqual(failure("10"))));
+    describe("when Loading", () => it("should return Success", () => expect(map((x) => +x)(loading())).toEqual(loading())));
+  });
+  describe("map2", () => {
+    describe("when all are Succeed", () =>
+      it("should return Success", () => expect(map2((x, y) => x + y)(success(10))(success(10))).toEqual(success(20))));
+    describe("when one is NotAsked", () => it("should return NotAsked", () => expect(map2((x) => +x)(success(10))(notAsked())).toEqual(notAsked())));
+    describe("when one is  Failure", () =>
+      it("should return Failure", () => expect(map2((x) => +x)(failure(10))(success("10"))).toEqual(failure(10))));
+    describe("when one is Loading", () => it("should return Success", () => expect(map2((x) => +x)(success(10))(loading())).toEqual(loading())));
+    describe("when all are FailState", () => it("should return Success", () => expect(map2((x) => +x)(loading())(notAsked())).toEqual(loading())));
+  });
+  describe("withDefault", () => {
+    describe("when Succeed", () => it("should return Success value", () => expect(withDefault("10")(success("20"))).toEqual("20")));
+    describe("when NotAsked", () => it("should return defaultValue", () => expect(withDefault("10")(notAsked())).toEqual("10")));
+    describe("when Failure", () => it("should return defaultValue", () => expect(withDefault("10")(failure("20"))).toEqual("10")));
+    describe("when Loading", () => it("should return defaultValue", () => expect(withDefault("10")(loading())).toEqual("10")));
+  });
   describe("fold", () => {
-    describe("when isLoading", () => {
-      it("should return whenLoading", () => {
-        const remoteData = loading();
+    describe("when isLoading", () =>
+      it("should return whenLoading", () =>
         expect(
           fold(
             () => 1,
             () => 0,
             () => 1,
             () => 1
-          )(remoteData)
-        ).toBe(0);
-      });
-    });
+          )(loading())
+        ).toBe(0)));
     describe("when isSuccess", () => {
       it("should return whenLoading", () => {
         const expectedValue = "Youpi";
--- src/remote-data.ts
+++ src/remote-data.ts
@@ -1,26 +1,95 @@
-export const notAsked = (): RemoteData<unknown, unknown> => ({
+/**
+ * Frequently when you're fetching data from an API, you want to represent four different states:
+ *
+ * NotAsked - We haven't asked for the data yet.
+ *
+ * Loading - We've asked, but haven't got an answer yet.
+ *
+ * Failure - We asked, but something went wrong. Here's the error.
+ *
+ * Success - Everything worked, and here's the data.
+ */
+export type RemoteData<A, E> = NotAsked | Loading | Success<A> | Failure<E>;
+
+/**   Initial NotAsked into the realm of RemoteData.
+ **/
+export const notAsked = <A, E>(): RemoteData<A, E> => ({
   _tag: "NotAsked",
 });
-export const loading = (): RemoteData<unknown, unknown> => ({
+/**  Initial Loading into the realm of RemoteData.
+ **/
+export const loading = <A, E>(): RemoteData<A, E> => ({
   _tag: "Loading",
 });
-export const success = <A>(value: A): RemoteData<A, unknown> => ({
+
+/**  Lift an ordinary value into the realm of RemoteData.
+ **/
+export const success = <A, B>(value: A): RemoteData<A, B> => ({
   _tag: "Success",
   value,
 });
-export const failure = <E>(value: E): RemoteData<unknown, E> => ({
+
+/**  Lift an error into the realm of RemoteData.
+ **/
+export const failure = <A, E>(value: E): RemoteData<A, E> => ({
   _tag: "Failure",
   value,
 });
 
-export type Success<A> = { value: A; _tag: "Success" };
-export type Failure<E> = { value: E; _tag: "Failure" };
-export type NotAsked = { _tag: "NotAsked" };
-export type Loading = { _tag: "Loading" };
+type Success<A> = { value: A; _tag: "Success" };
+type Failure<E> = { value: E; _tag: "Failure" };
+type NotAsked = { _tag: "NotAsked" };
+type Loading = { _tag: "Loading" };
 
-export type RemoteData<A, E> = NotAsked | Loading | Success<A> | Failure<E>;
+/** Map a function into the Success value.
+ * @param whenSucceed Function to map the succeed value
+ * @returns Function thats take the remote data
+ * @param remoteData The remotedata to map
+ * @returns The return new states ,if succeed state return mapped state
+ * @example map(item => items.filter(predicate)))(loading()) === loading()
+ * @example map(item => +item)(success("10")) === success(10)
+ */
+export const map =
+  <A, R>(whenSucceed: (A) => R) =>
+  (rd: RemoteData<A, unknown>) => {
+    if (isSuccess(rd)) return success(whenSucceed(rd.value));
+    return rd;
+  };
 
-export const foldw =
+/** Combine two remote data sources with the given function. The result will succeed when (and if) both sources succeed. If not return de failure one and if its 2 failure return the first one.
+ * @param whenSucceed Function to map the succeed value
+ * @returns Function thats take the first RemoteData
+ * @param remoteData First RemoteData
+ * @returns Function thats take the remote second RemoteData
+ * @param remoteData2 Second RemoteData
+ * @returns The return new states ,if succeed state return mapped state
+ * @example map2((item1, item2) => item1 + item2)(loading())(success(10)) === loading()
+ * @example map2((item1, item2) => item1 + item2)(success(10))(success(10)) === success(20)
+ * @example map2((item1, item2) => item1 + item2)(notAsked())(loading()) === notAsked()
+ */
+export const map2 =
+  <A, B, R>(whenSucceed: (A, B) => R) =>
+  (rd: RemoteData<A, unknown>) =>
+  (rd2: RemoteData<B, unknown>) => {
+    if (isSuccess(rd) && isSuccess(rd2)) return success(whenSucceed(rd.value, rd2.value));
+    if (isNotAsked(rd) || isLoading(rd) || isFailure(rd)) return rd;
+    return rd2;
+  };
+
+/** Return the Success value, or the default.
+ * @param defaultValue The value returned if is not Succeed
+ * @returns Function thats take the RemoteData
+ * @param remoteData RemoteData
+ * @returns The default value or the success value
+ * @example withDefault("Not Success RemoteData")(loading()) === "Not Success RemoteData"
+ * @example withDefault("Not Success RemoteData")(success("10")) === success("10")
+ */
+export const withDefault =
+  <A>(defaultValue: A) =>
+  (rd: RemoteData<A, unknown>) =>
+    isSuccess(rd) ? rd.value : defaultValue;
+
+const foldw =
   <A, E, R>(whenNotAsked: () => R, whenLoading: () => R, whenSuccess: (a: A) => R, whenFailure: (e: E) => R) =>
   (rd: RemoteData<A, E>) => {
     if (isNotAsked(rd)) return whenNotAsked();
@@ -29,6 +98,40 @@ export const foldw =
     if (isFailure(rd)) return whenFailure(rd.value);
   };
 
+/** Extract data for each state.
+ * @param whenNotAsked Function when is state is NotAsked.
+ * @param whenLoading Function when is state is Loading.
+ * @param whenSuccess Function when is state is Success.
+ * @param whenFailure Function when is state is Failure.
+ * @returns Function thats take the remote data
+ * @param remoteData The remotedata to extract
+ * @returns The right result of the current state
+ * @example fold(
+                () => <p> Not Asked yet </p>, 
+                () => <Loader />,
+                (items: List<Item>) => <> {items.map(\i -> <Item item={i}/>} </>,
+                (_ : AxiosError) => <p> Something bad happen! Call the 911 </p> 
+          )(notAsked()) === <p> Not Asked yet </p>
+ * @example fold(
+                () => <p> Not Asked yet </p>, 
+                () => <Loader />,
+                (items: List<Item>) => <> {items.map(\i -> <Item item={i}/>} </>,
+                (_ : AxiosError) => <p> Something bad happen! Call the 911 </p> 
+          )(loading()) === <Loader />,
+ * @example fold(
+                () => <p> Not Asked yet </p>, // imposible state comme ça
+                () => <Loader />,
+                (items: List<Item>) => <> {items.map(\i -> <Item item={i}/>} </>,
+                (_ : AxiosError) => <p> Something bad happen! Call the 911 </p> 
+          )(success([items1])) === <> {items.map(\i -> <Item item={i}/>} </>
+ * @example fold(
+                () => <p> Not Asked yet </p>, // imposible state comme ça
+                () => <Loader />,
+                (items: List<Item>) => <> {items.map(\i -> <Item item={i}/>} </>,
+                (_ : AxiosError) => <p> Something bad happen! Call the 911 </p> 
+          )(failure(anyError)) === <p> Something bad happen! Call the 911 </p> 
+
+ */
 export const fold: <A, E, R>(
   whenNotAsked: () => R,
   whenLoading: () => R,
